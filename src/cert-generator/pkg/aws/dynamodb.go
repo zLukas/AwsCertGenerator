5d53bc440cd72ee666817a87973661546990e1b5
zLukas <xlukas9@gmail.com>
2023-10-02 19:40:00 +0200

    progress save

--- src/cert-generator/pkg/aws/dynamodb.go
+++ src/cert-generator/pkg/aws/dynamodb.go
@@ -1,1 +1,10 @@
 package aws
+
+import "github.com/aws/aws-sdk-go/aws/session"
+
+func LogIntoDb(client dBClient) *session.Session {
+	sess := client.Must(client.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	return sess
+}
--- src/cert-generator/pkg/aws/types.go
+++ src/cert-generator/pkg/aws/types.go
@@ -1,1 +1,22 @@
 package aws
+
+import (
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+type CertItem struct {
+	PrivateKey string
+	Cert       string
+}
+
+type TableRecord struct {
+	CaCert       CertItem
+	CeCert       CertItem
+	issuer       string
+	creationDate string
+}
+
+type dBClient interface {
+	Must(sess *session.Session, err error) *session.Session
+	NewSessionWithOptions(opts session.Options) (*session.Session, error)
+}
